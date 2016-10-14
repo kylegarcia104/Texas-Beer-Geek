43820732cd311ac00daccea075dca92bad6a7db0
Kevin <kevin.e.quintero@gmail.com>
2016-10-14 14:19:37 -0500

    Initial Command

---
+++ texasbeergeek/js/pie.js
@@ -0,0 +1,95 @@
+
+    var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100);
+    };
+    var randomColorFactor = function() {
+        return Math.round(Math.random() * 255);
+    };
+    var randomColor = function(opacity) {
+        return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+    };
+
+    var config = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: [
+                    260,
+                    101,
+                    58,
+                    33,
+                    31,
+                    142,
+                ],
+                backgroundColor: [
+                    "#F7464A",
+                    "#46BFBD",
+                    "#FDB45C",
+                    "#949FB1",
+                    "#4D5360",
+                ],
+            }, {
+                data: [
+                    0, 0, 0, 0, 0,
+                ],
+                backgroundColor: [
+                    "#F7464A",
+                    "#46BFBD",
+                    "#FDB45C",
+                    "#949FB1",
+                    "#4D5360",
+                ],
+            }, {
+                data: [
+                 25, 25, 25 ,25, 
+                ],
+                backgroundColor: [
+                    "grey",
+                    "grey",
+                    "grey",
+                    "grey",
+                ],
+            }],
+            labels: [
+                "Red - USA",
+                "Green - China",
+                "Yellow - Germany",
+                "Grey - France",
+                "Dark Grey - UK",
+                "White-ish - Other"
+            ]
+        },
+        options: {
+            responsive: true
+        }
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("chart-area").getContext("2d");
+        window.myPie = new Chart(ctx, config);
+    };
+
+    $('#randomizeData').click(function() {
+        $.each(config.data.datasets, function(i, piece) {
+            $.each(piece.data, function(j, value) {
+                config.data.datasets[i].data[j] = randomScalingFactor();
+                config.data.datasets[i].backgroundColor[j] = randomColor(0.7);
+            });
+        });
+        window.myPie.update();
+    });
+
+    $('#addDataset').click(function() {
+        var newDataset = {
+            backgroundColor: [randomColor(0.7), randomColor(0.7), randomColor(0.7), randomColor(0.7), randomColor(0.7)],
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        };
+
+        config.data.datasets.push(newDataset);
+        window.myPie.update();
+    });
+
+    $('#removeDataset').click(function() {
+        config.data.datasets.splice(0, 1);
+        window.myPie.update();
+    });
